2299a8321f6ceab4d5e742cad32461251ea3cfbe
Debbl <me@aiwan.run>
2022-07-31 15:35:13 +0800

    fix cors

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:5173/api/";
+// const BASE_URL = "http://127.0.0.1:5173/api/";
+const BASE_URL = "http://localhost:5173/api/";
 const TIMEOUT = 2000;
 
 export { BASE_URL, TIMEOUT };
